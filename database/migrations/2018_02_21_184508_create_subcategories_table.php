3e7f4f60d8e67d53e5906c7cfaba2c527d290be4
Samuel <31468856+samuelhank@users.noreply.github.com>
2018-03-29 17:12:24 +0300

    db configuration

---
+++ database/migrations/2018_02_21_184508_create_subcategories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubcategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcategories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('subname');
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subcategories');
+
+    }
+}
